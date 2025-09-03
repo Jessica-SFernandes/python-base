37a1d67b11337dd39fcc34942ef05b999f69ba45
Jessica Fernandes <fernandesjessica333@gmail.com>
2025-09-03 18:40:04 -0300

    Multilinguas

--- hello.py
+++ hello.py
@@ -1,1 +1,33 @@
-print('Hello, World!')
+#!/usr/bin/env python3
+""" Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o 
+programa exibe a mensagem correnspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+    
+    export LANG=pt_BR.UTF-8
+
+Execução:
+    $ python3 hello.py 
+    ou 
+    $ ./hello.py
+
+"""
+__version__ = "0.0.1"
+__authot__ = "Jessica Fernandes"
+__license__ = "Unlicense"
+
+import os 
+
+current_language = os.getenv("LANG")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
